c83b5d78cc05517636f2b16d32672e6b7974dfed
nathb <nathb2b@gmail.com>
2019-02-03 11:46:39 +0100

    ^

--- samples/Comp.js
+++ samples/Comp.js
@@ -24,7 +24,6 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/
