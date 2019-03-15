02c6b4a52066f8201bec45a11b87b9341d01276d
nathb <nathb2b@gmail.com>
2019-03-15 17:43:33 +0100

    ^

--- src/asTweener.js
+++ src/asTweener.js
@@ -508,6 +508,7 @@ export default function asTweener( ...argz ) {
 						},
 						'drop': ( e, touch, descr ) => {
 							lastPos = null;
+							
 						}
 					}, null,
 					opts.enableMouseDrag
