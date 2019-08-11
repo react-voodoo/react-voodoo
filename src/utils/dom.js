8e544a12d0daf68547b28512c21adaca00fe5ffc
nathb <nathb2b@gmail.com>
2019-08-11 18:31:27 +0200

    ^

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -31,7 +31,6 @@ let
 		// include the 'heartz' as a way to have a non matching MQ to help terminate the join
 		// https://git.io/vznFH
 		var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-		alert(":" + mq(query))
 		return mq(query);
 	}()),
 	_dom      = isBrowser ? {
